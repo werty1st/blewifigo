e5f68e0034755fbe2e046cb3312a55291e27f9e8
ronny <werty1st@gmail.com>
2019-08-30 20:26:19 +0200

    scheisse

--- helper/wpa.go
+++ helper/wpa.go
@@ -1,6 +1,13 @@
 package service
 
+import (
+	"github.com/Wifx/gonetworkmanager/NetworkManager"
+)
+
 //https://github.com/BellerophonMobile/gonetworkmanager
 func WpaService() {
+
+	var devices []Device
+	devices = NetworkManager.GetDevices()
 
 }
